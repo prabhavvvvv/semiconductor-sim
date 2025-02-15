2751a4ce775264234454206072cad46a9ae7f48f
prabhaaaav <108563497+prabhavvvvv@users.noreply.github.com>
2025-02-15 14:35:56 -0800

    Add files via upload

---
+++ game.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    root.title("Digital Circuit Simulator")
+
+    # Set the window size
+    root.geometry("600x400")
+
+    # TODO: Add widgets/drawings here
+
+    # Start the Tkinter loop
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
